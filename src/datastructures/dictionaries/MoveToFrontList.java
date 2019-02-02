7ae6cccb94346ee3a8c583cf8ce8fbe0940236f6
M8yki <M8yki@d-10-19-226-33.dhcp4.washington.edu>
2019-02-02 15:26:01 -0800

    Implemented Iterator, insert, and find, but not sure where error is.

--- src/datastructures/dictionaries/MoveToFrontList.java
+++ src/datastructures/dictionaries/MoveToFrontList.java
@@ -1,9 +1,9 @@
 package datastructures.dictionaries;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import cse332.datastructures.containers.*;
-import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.DeletelessDictionary;
 
 /**
@@ -20,49 +20,93 @@ import cse332.interfaces.misc.DeletelessDictionary;
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
     private Node front;
-
+    
     private class Node {
-        private V data;
         private Node next;
+        private Item<K, V> item;
 
-        public Node(V data) {
-            this.data = data;
+        public Node(Item<K, V> item) {
+            this.item = item;
             this.next = null;
-        }
-
-        public Node(V data, Node next) {
-            this.data = data;
-            this.next = next;
         }
     }
     
     @Override
     public V insert(K key, V value) {
-        V location = find(key);
-        front = delete(front, item);
-        first = new Node(item, front);
-        return location;
+        V data = find(key); // Data at key
+        if (data == null) { // Inserts at front since no key existed before
+            Node newFront = new Node(new Item(key, value));
+            newFront.next = front;
+            front = newFront;
+        } else { // Accesses and moves to front
+            boolean keyFound = false;
+            Node prev = null;
+            Node curr = front;
+            while (!keyFound) {
+                if (curr.item.key.equals(key)) {
+                    curr.item.value = value;
+                    data = value;
+                    if (prev != null) {
+                        prev.next = curr.next;
+                        curr.next = front;
+                        front = curr;
+                    }
+                    keyFound = true;
+                } else {
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+        }
+        return data;
     }
 
     @Override
     public V find(K key) {
-        int i = 1;
-        Node x = new
-        while(Node x = front; x != null; x = x.next) {
-            if (x.item == key) {
-                return i;
+        V data = null;
+        Node prev = null;
+        Node curr = front;
+        boolean keyFound = false;
+        while (curr != null && !keyFound) {
+            if (curr.item.key.equals(key)) { // If key found
+                data = curr.item.value;
+                keyFound = true;
+                if (prev != null) {     // Link found item to front, only if not first item
+                    prev.next = curr.next;
+                    curr.next = front;
+                    front = curr;
+                }
+            } else {
+                prev = curr;            // Continue searching
+                curr = curr.next;
             }
-            i++;
         }
-        return 0;
+        return data;
     }
-    
-//    private boolean isEmpty() {
-//        return front == null;
-//    }
 
     @Override
     public Iterator<Item<K, V>> iterator() {
-        throw new NotYetImplementedException();
+        return new ListIterator();
+    }
+    
+    private class ListIterator implements Iterator<Item<K, V>> {
+        private Node currNode;
+        
+        public ListIterator() {
+            currNode = front;
+        }
+        
+        public boolean hasNext() {
+            return currNode != null;
+        }
+        
+        public Item<K, V> next() {
+            if (hasNext()) {
+                Item<K, V> curr = currNode.item;
+                currNode = currNode.next;
+                return curr;
+            }
+            throw new NoSuchElementException();
+        }
     }
 }
