590bbaf46e97bc58ecc58cc55f812c9019a15001
Mayki <Mayki@10.19.218.234>
2019-02-16 12:37:35 -0800

    Implemented HeapSort, passed tests.

--- src/p2/sorts/HeapSort.java
+++ src/p2/sorts/HeapSort.java
@@ -2,13 +2,21 @@ package p2.sorts;
 
 import java.util.Comparator;
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 public class HeapSort {
     public static <E extends Comparable<E>> void sort(E[] array) {
         sort(array, (x, y) -> x.compareTo(y));
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public static <E> void sort(E[] array, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap sortedHeap = new MinFourHeap();
+        for (E item : array) {
+            sortedHeap.add((Comparable) item);
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (E) sortedHeap.next();
+        }
     }
 }
